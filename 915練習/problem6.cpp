9ce4a89e32dbc971f86e3b8a53e313e201cfee5d
ch97thomas <ch97thomas@gmail.com>
2026-09-22 14:24:58 +0800

    這是我的第一次提交,915練習

---
+++ 915練習/problem6.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	char a,b,c;
+	cin>>a>>b>>c;
+	int sum=0;
+	sum+=(a-'0')+(b-'0')+(c-'0');
+	cout<<sum;
+}
---
+++ 915練習/problem7.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a;
+	cin>>a;
+	if(a%11==0){
+		cout<<a<<" is a multiple of 11.";
+	}else{
+		cout<<a<<" is not a multiple of 11.";
+	}
+	return 0;
+}
